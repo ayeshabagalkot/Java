32678f331d492d0db76782a5d9bf40087e3bb67c
ayeshabagalkot <ayeshabagalkot@gmail.com>
2025-04-09 10:12:04 +0530

    First Java Prg

---
+++ first.java
@@ -0,0 +1,12 @@
+class demo{
+    public void show(){
+        System.out.println("My first Java Program for DevOps");
+    }
+}
+
+class first{
+    public static void main(String[] args) {
+        demo d1 = new demo();
+        d1.show();
+    }
+}
